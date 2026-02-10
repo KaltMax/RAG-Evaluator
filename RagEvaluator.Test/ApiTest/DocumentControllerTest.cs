5a14796970018c6c0518d4915dbc8e07d76df5fe
KaltMax <mkaltenr@gmx.at>
2026-02-10 21:11:40 +0100

    xUnit pipeline test

---
+++ RagEvaluator.Test/ApiTest/DocumentControllerTest.cs
@@ -0,0 +1,11 @@
+﻿namespace RagEvaluator.Test.ApiTest
+{
+    public class DocumentControllerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
